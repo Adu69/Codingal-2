db3cd78c84b8bcc47d031fb78ec82007da815535
Adu69 <83687446+Adu69@users.noreply.github.com>
2025-08-28 21:55:52 +0530

    Add files via upload

---
+++ Circle area and diameter.py
@@ -0,0 +1,4 @@
+radius = int(input("Enter the radius of the circle: "))
+area = 1.14159 * (radius * radius)
+diameter = 2 * radius
+print("The area of the circle is", area, "and the diameter of the circle is", diameter)
